e03ac56c2ee90ed5d41df9e6f5d71d2b080f609b
Melissa Longenberger <melissalongenberger24@gmail.com>
2024-04-21 13:55:48 -0400

    saving

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import './App.css'
-// import {Routes, Route} from 'react-router-dom'
-import projects from "./components/json/projects.json"
-import Proj from './components/ProjCard';
+import {Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Home from './components/Home'
 
 
 function App() {
 
-  const completed_projects = projects.filter(project => project.is_finished)
-  const started_projects = projects.filter(project => project.is_started)
 
   return (
     <>
       <Header />
       <main>
-        {started_projects.length == 0 ?
-        <div>Not projects started yet</div>
-        :
-        <div>
-          <h2>Started Projects</h2>
-          <Proj projects={started_projects} />
-        </div>
-        }
-        {completed_projects.length == 0 ? 
-        <div>No finished projects yet</div>
-        :
-        <div>
-          <h2>Finished Projects</h2>
-          <Proj projects={ completed_projects } />
-        </div>
-        }
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
       </main>
       <Footer />
     </>
---
+++ src/components/Home.jsx
@@ -0,0 +1,48 @@
+import projects from "./json/projects.json"
+import Proj from './ProjCard';
+
+
+const Home = () => {
+
+    const completed_projects = projects.filter(project => project.is_finished)
+    const started_projects = projects.filter(project => project.is_started)
+    const coming_projects = projects.filter(project => project.is_idea)
+
+
+    return (
+        <>
+            <h2>Completed Projects</h2>
+            {completed_projects.length == 0 ?
+                <div>
+                    <h3>No projects finished yet</h3>
+                </div>
+                :
+                <div>
+                    <Proj projects={completed_projects} />
+                </div>
+            }
+            <h2>Projects that have been Started</h2>
+            {started_projects.length == 0 ?
+                <div>
+                    <h3>No projects started yet</h3>
+                </div>
+                :
+                <div>
+                    <Proj projects={started_projects} />
+                </div>
+            }
+            <h2>Project ideas</h2>
+            {coming_projects.length == 0 ?
+                <div>
+                    <h3>No more project Ideas at this time</h3>
+                </div>
+                :
+                <div>
+                    <Proj projects={coming_projects} />
+                </div>
+            }
+        </>
+    )
+}
+
+export default Home
