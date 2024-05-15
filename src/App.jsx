f25ea45fb6c098c1fd2a263ca2a0823147ef6512
Melissa Longenberger <melissalongenberger24@gmail.com>
2024-05-15 13:42:58 -0400

    moved files around

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {Routes, Route} from 'react-router-dom'
-import './static/css/style.css'
+import './styles/style.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './views/Home'
