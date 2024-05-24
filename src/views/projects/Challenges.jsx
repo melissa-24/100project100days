065de43626af90d3757e5849bff0c1eeb2b94d6a
Melissa Longenberger <melissalongenberger24@gmail.com>
2024-05-23 23:13:03 -0400

    updated challenges project

--- src/views/projects/Challenges.jsx
+++ src/views/projects/Challenges.jsx
@@ -42,17 +42,15 @@ const Challenges = () => {
     const totalOther = totalSolved - (totalJS + totalPy)
 
 
-
-
     return (
         <>
         <h2>Total Challenges Solved as of { today }: { totalSolved }</h2>
         <ChallengesVSCard one="JavaScript" two="Python" three="Other Languages" countOne={totalJS} countTwo={totalPy} countThree={totalOther} />
         <div className="challCard">
             <ChallengesCard header={cw} theChallenges={solved} />
-            {/* <ChallengesCard header={hr} count={total_hr} jsSolved={total_hr_js} pySolved={total_hr_py} theChallenges={the_hr} />
-            <ChallengesCard header={lc} count={total_lc} jsSolved={total_lc_js} pySolved={total_lc_py} theChallenges={the_lc} />
-            <ChallengesCard header={misc} count={total_misc} jsSolved={total_misc_js} pySolved={total_misc_py} theChallenges={the_misc} /> */}
+            <ChallengesCard header={hr} theChallenges={solved} />
+            <ChallengesCard header={lc} theChallenges={solved} />
+            <ChallengesCard header={misc} theChallenges={solved} />
         </div>
         </>
         
