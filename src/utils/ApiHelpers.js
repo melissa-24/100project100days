f807018da7b97c37f29628288fe5ee15ed1afac8
Melissa Longenberger <melissalongenberger24@gmail.com>
2024-05-23 21:55:37 -0400

    displaying/getting json data from challenges repo

--- src/utils/ApiHelpers.js
+++ src/utils/ApiHelpers.js
@@ -1,16 +1,18 @@
 import axios from 'axios'
 
 const BASE_URL = 'https://ninja-api.navyladyveteran.com/api/';
+const CHALLENGES_JSON = 'https://raw.githubusercontent.com/'
 
 const ENDPOINTS = {
     getCategories: '/categories',
     getImages: '/images',
     getMedium: '/mediums',
-    getSubCategories: '/subCategories'
-    // Add more endpoints as needed
+    getSubCategories: '/subCategories',
+    getChallengesJson: '/melissa-24/codeChallenges/main/challengeData.json'
 };
 
 const createUrl = (endpoint) => `${BASE_URL}${endpoint}`;
+const createAltUrl = (endpoint) => `${CHALLENGES_JSON}${endpoint}`
 
 
 export const get = async(endpoint, params = {}) => {
@@ -22,10 +24,20 @@ export const get = async(endpoint, params = {}) => {
         console.log(error)
     }
 }
+export const challengesGet = async(endpoint, params = {}) => {
+    try{
+        const res = await axios.get(createAltUrl(endpoint), {params})
+        return res.data
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
 
 export const API = {
     getCategories: () => get(ENDPOINTS.getCategories),
     getImages: () => get(ENDPOINTS.getImages),
     getMedium: () => get(ENDPOINTS.getMedium),
-    getSubCategories: () => get(ENDPOINTS.getSubCategories)
+    getSubCategories: () => get(ENDPOINTS.getSubCategories),
+    getChallengesJson: () => challengesGet(ENDPOINTS.getChallengesJson)
 };
--- src/views/projects/Challenges.jsx
+++ src/views/projects/Challenges.jsx
@@ -3,6 +3,8 @@ import { is_length, lang, source } from "../../utils/ChallengeHelpers"
 import { getDate } from "../../utils/TimeHelpers"
 import ChallengesVSCard from "../../components/cards/challengesCards/ChallengesVSCard"
 import ChallengesCard from "../../components/cards/challengesCards/ChallengeCard"
+import { API } from "../../utils/ApiHelpers"
+import { useEffect, useState} from 'react'
 
 
 const Challenges = () => {
@@ -47,21 +49,39 @@ const Challenges = () => {
     const total_misc_py = is_length(misc_py)
     const total_misc_js = is_length(misc_js)
 
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
 
+    useEffect(() => {
+        API.getChallengesJson()
+        .then(res=> {
+            setData(res)
+            setLoading(false)
+        })
+        .catch(error => {
+            setError(error)
+            setLoading(false)
+        })
+    }, [])
 
+    console.log('data', data, 'loading', loading, 'error', error)
 
+    if (loading) return <div>Loading ...</div>
+    if (error) return <div>Error: {error.message}</div>
 
 
     return (
         <>
-        <h2>Total Challenges Solved as of { today }: { total_solved }</h2>
+        {/* <h2>Total Challenges Solved as of { today }: { total_solved }</h2>
         <ChallengesVSCard one={js} two={py} countOne={total_js} countTwo={total_py} />
         <div className="challCard">
             <ChallengesCard header={cw} count={total_cw} jsSolved={total_cw_js} pySolved={total_cw_py} theChallenges={the_cw} />
             <ChallengesCard header={hr} count={total_hr} jsSolved={total_hr_js} pySolved={total_hr_py} theChallenges={the_hr} />
             <ChallengesCard header={lc} count={total_lc} jsSolved={total_lc_js} pySolved={total_lc_py} theChallenges={the_lc} />
             <ChallengesCard header={misc} count={total_misc} jsSolved={total_misc_js} pySolved={total_misc_py} theChallenges={the_misc} />
-        </div>
+        </div> */}
+        {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
         </>
         
     )
