4dc6f606672e7e6c8d489af11fa7c854318c3a22
Melissa Longenberger <melissalongenberger24@gmail.com>
2024-05-29 12:47:54 -0400

    updating and starting the git project over

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import {Routes, Route} from 'react-router-dom'
-import backgroundImage from './assets/hexellence.png'
 import './styles/style.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './views/Home'
 import Logs from './views/Log'
-import PageOne from './views/ProjectPageOne'
 import BackgroundChanger from './views/projects/BackgroundChanger'
 import ImportantDates from './views/projects/ImportantDates'
 import Challenges from './views/projects/Challenges'
 import CssImage from './views/projects/CssImage'
 import ImagesApi from './views/projects/ImagesApi'
-import GitCommitGraph from './views/projects/GitCommitGraph'
-import GitContributionsGraph from './views/projects/GitContributionsGraph'
-import GitRepos from './views/projects/GitRepos'
 
 function App() {
 
@@ -25,16 +20,12 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/logs" element={<Logs />} />
-          {/* <Route path="/project_page_one" element={<PageOne />} /> */}
           <Route path="projects/2" element={<Logs />} />
           <Route path="/projects/3" element={<BackgroundChanger />} />
           <Route path="/projects/5" element={<ImagesApi />} />
           <Route path="/projects/9" element={<ImportantDates />} />
           <Route path="/projects/18" element={<Challenges />} />
           <Route path="/projects/19" element={< CssImage />} />
-          {/* <Route path="/projects/20" element={< GitCommitGraph />} /> */}
-          <Route path="/projects/20" element={<GitContributionsGraph />} />
-          <Route path="/projects/21" element={<GitRepos />} />
         </Routes>
       </main>
       <Footer />
