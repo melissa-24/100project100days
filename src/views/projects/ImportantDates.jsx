8e555e1986e222d893f4f4cd5dc6089f9f50af82
melissa-24 <melissalongenberger24@gmail.com>
2025-02-18 17:38:46 -0500

    updated dates and other files

--- src/views/projects/ImportantDates.jsx
+++ src/views/projects/ImportantDates.jsx
@@ -7,8 +7,7 @@ const ImportantDates = () => {
     return (
         <>
         <h2>The Current Time is: { current_time }</h2>
-        <h2>Based off of a site I made for my Anniversary this one is all about the important dates in my life</h2>
-        <p>If you would like to see the original site I made here is the link: <a href="https://melissa-24.github.io/anniversaryHusband/" target="_blank" rel="noopener noreferrer">Anniversary Site</a></p>
+
         <DaysCard filterBy={"Event"} />
         <DaysCard filterBy={"Graduation"} />
         <DaysCard filterBy={"Birthday"} />
